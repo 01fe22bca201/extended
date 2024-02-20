56fa0788eeb4c31b1f965e2532b7a7875f167c01
vaishnavi <01fe22bca201@kletech.ac.in>
2024-02-20 10:46:08 +0530

    extended calculator

---
+++ extendedCalculator.java
@@ -0,0 +1,10 @@
+public class extendedCalculator {
+    public float square(float operand_1){
+        return operand_1*operand_1;
+    }
+    public float cube(float operand_1){
+        return operand_1*operand_1*operand_1;
+    }
+}
+
+
